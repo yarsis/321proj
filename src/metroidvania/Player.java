535c7fda370a4c0d5b7392f9bb596b01eee71692
Henry Schulz <User@10.14.0.9>
2021-11-10 10:51:25 -0600

    Moved level progression to PlayPanel and added player health.

--- src/metroidvania/Player.java
+++ src/metroidvania/Player.java
@@ -16,6 +16,7 @@ public class Player {
     PlayPanel game;
     int x, y;
     int width, height;
+    int health;
     double xspeed, yspeed;
     Rectangle hitBox;
     boolean keyLeft, keyRight, keyDown, keyUp;
@@ -26,6 +27,7 @@ public class Player {
         this.y = y;
         width = 50;
         height = 100;
+        health = 100;
         
         hitBox = new Rectangle(x, y, width, height);
     }
@@ -41,12 +43,10 @@ public class Player {
             xspeed *= 0.7;
         }
         else if(keyLeft && !keyRight){
-            xspeed -= 1;    //movement handlers
-            System.out.println("na");
+            xspeed -= 1;
         }
         else if(keyRight && !keyLeft){
             xspeed += 1;
-            System.out.println("ja");
         }
         
         //stop movement if going very slow (prevent gliding)
@@ -57,12 +57,12 @@ public class Player {
         if(7 < xspeed) xspeed = 7;
         if(xspeed < -7) xspeed = -7;
         
+        // Check if player is on ground
         if(keyUp){
             hitBox.y += 1;
             for(int i = 0; i < game.gameTerrain.size(); i++){
                 Terrain ter = game.gameTerrain.get(i);
                 if(ter.hitBox.intersects(hitBox)) yspeed = -8;
-                //check if terrain is under player, if so allow them to jump
             }
         }
         
@@ -110,27 +110,6 @@ public class Player {
                     y = hitBox.y;
                 }
         }
-        
-        // Player reached end of terrain
-        if(hitBox.x <= 10 && hitBox.y == 500) {
-            move(600, 500);
-            game.makeTerrain();
-        }
-        
-        // Horizontal limiter
-        if(hitBox.x <= 0 || 630 <= hitBox.x) {
-            xspeed = 0;
-        }
-        
-        // Ceiling limiter
-        if(hitBox.y <= 0) {
-            yspeed = 0;
-        }
-        
-        // Player fell off screen and died
-        if(700 <= hitBox.y) {
-            move(600, 500);
-        }
     }
     
     public void move(int x, int y){
@@ -147,8 +126,13 @@ public class Player {
     }
     
     public void drawPlayer(Graphics2D gtd){
+        /**
+        * Function will draw the player at its location.
+        * @param gtd        Display area for the game.
+        * @precondition     Display area exists.
+        * @postcondition    Player has been drawn.
+        */
         gtd.setColor(Color.PINK);
         gtd.fillRect(x, y, width, height);
-        //draw player as pink rectangle
     }
 }
