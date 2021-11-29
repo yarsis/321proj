308d0749be2506306cc8fac9bbdce9fa33061a02
Henry Schulz <User@10.14.0.9>
2021-11-29 17:59:59 -0600

    Added color menu.

--- src/metroidvania/MenuPanel.java
+++ src/metroidvania/MenuPanel.java
@@ -24,8 +24,16 @@ public class MenuPanel extends javax.swing.JPanel implements ActionListener {
     private Rectangle scoreRect;
     private Rectangle saveRect;
     private Rectangle exitRect;
+    private Rectangle colorsRect;
+    private Rectangle colorsPlayerRect;
+    private Rectangle colorsBackRect;
+    private Rectangle colorsEnemyRect;
+	private PlayPanel game;
+	private boolean settingsAccessed = false;
     
-    public MenuPanel() {
+    public MenuPanel(PlayPanel game) {
+		this.game = game;
+		
         newTimer = new Timer();
         newTimer.schedule(new TimerTask() {
             @Override
@@ -37,8 +45,7 @@ public class MenuPanel extends javax.swing.JPanel implements ActionListener {
     
     public void paintMain(Graphics g) {
         /**
-        * Function will draw the main menu with "play", "scores" and "exit" 
-        * buttons.
+        * Function draw the main menu with "play", "scores" and "exit" buttons.
         * @param g          I don't know.
         * @precondition     Game area exists.
         * @postcondition    Main menu is drawn.
@@ -71,20 +78,32 @@ public class MenuPanel extends javax.swing.JPanel implements ActionListener {
     
     public void paintPause(Graphics g) {
         /**
-        * Function will draw a pause menu with "continue" and "exit" buttons.
+        * Function will remove previous buttons and draw a pause menu with 
+		* "change colors", "continue" and "exit" buttons.
         * @param g          I don't know.
         * @precondition     Game area exists.
         * @postcondition    Pause menu is drawn.
         */
         super.paint(g); //prevent flickering
         Graphics2D gtd = (Graphics2D) g;
+		
+		if(settingsAccessed) {
+		colorsRect.setSize(0, 0);
+		colorsPlayerRect.setSize(0, 0);
+		colorsBackRect.setSize(0, 0);
+		colorsEnemyRect.setSize(0, 0);
+		settingsAccessed = false;
+		}
         
+		colorsRect = new Rectangle(275, 280, 150, 50);
         playRect = new Rectangle(275, 340, 150, 50);
         exitRect = new Rectangle(275, 400, 150, 50);
         
+		gtd.drawRect(275, 280, 150, 50);
         gtd.drawRect(275, 340, 150, 50);
         gtd.drawRect(275, 400, 150, 50);
         gtd.setColor(Color.WHITE);
+		gtd.fillRect(276, 281, 149, 49);
         gtd.fillRect(276, 341, 149, 49);
         gtd.fillRect(276, 401, 149, 49);
         
@@ -94,33 +113,74 @@ public class MenuPanel extends javax.swing.JPanel implements ActionListener {
         
         gtd.setColor(Color.BLACK);
         g.setFont(new Font("Ariel", Font.PLAIN, 12));
+		gtd.drawString("CHANGE COLORS", 300, 310);
         gtd.drawString("CONTINUE", 320, 370);
         gtd.drawString("EXIT", 338, 430);
     }
     
+    public void paintSettings(Graphics g) {
+        /**
+        * Function will remove previous buttons and draw a settings menu with 
+		* "player color", "background color" and "enemy color" buttons with 
+		* their respective colors.
+        * @param g          I don't know.
+        * @precondition     Game area exists.
+        * @postcondition    Settings menu is drawn.
+        */
+        super.paint(g); //prevent flickering
+        Graphics2D gtd = (Graphics2D) g;
+		settingsAccessed = true;
+		
+		playRect.setSize(0, 0);
+		scoreRect.setSize(0, 0);
+		exitRect.setSize(0, 0);
+		colorsRect.setSize(0, 0);
+        
+		colorsPlayerRect = new Rectangle(275, 280, 150, 50);
+        colorsBackRect = new Rectangle(275, 340, 150, 50);
+        colorsEnemyRect = new Rectangle(275, 400, 150, 50);
+        
+		gtd.drawRect(275, 280, 150, 50);
+        gtd.drawRect(275, 340, 150, 50);
+        gtd.drawRect(275, 400, 150, 50);
+        gtd.setColor(Color.WHITE);
+		gtd.fillRect(276, 281, 149, 49);
+        gtd.fillRect(276, 341, 149, 49);
+        gtd.fillRect(276, 401, 149, 49);
+        
+        g.setFont(new Font("Ariel", Font.PLAIN, 77));
+        gtd.setColor(Color.LIGHT_GRAY);
+        gtd.drawString("SETTINGS", 165, 180);
+        
+        gtd.setColor(game.getPlayerColor());
+        g.setFont(new Font("Ariel", Font.PLAIN, 12));
+		gtd.drawString("PLAYER COLOR", 305, 310);
+        gtd.setColor(game.getBackgroundColor());
+        gtd.drawString("BACKGROUND COLOR", 285, 370);
+        gtd.setColor(game.getEnemyColor());
+        gtd.drawString("ENEMY COLOR", 310, 430);
+    }
+    
     public String switchPauseState(String state) {
         /**
-        * Function will change the game state from pause to play and vice-versa, 
-        * if applicable.
-        * @param state      String that represents the current state of the game.
+        * Function will change the game state to pause. If the game is in any 
+		* other state, return to game state.
+        * @param state      String that represents the current state of the 
+		* game.
         * @postcondition    Game state was changed.
         * @return           String representing new game state.
         */
-        if ("pause".equals(state)) {
-            return "game";
-        }
-        
         if ("game".equals(state)) {
             return "pause";
         }
         
-        return state;
+        return "game";
     }
     
     public void paintGameOver(int points, Graphics g) {
         /**
-        * Function will draw a game over menu with "save score" and "exit" 
-        * buttons.
+        * Function will remove previous buttons and draw a game over menu with 
+		* "save score" and "exit" buttons.
         * @param g          I don't know.
         * @precondition     Game area exists.
         * @postcondition    Game over menu is drawn.
@@ -128,6 +188,13 @@ public class MenuPanel extends javax.swing.JPanel implements ActionListener {
         super.paint(g); //prevent flickering
         Graphics2D gtd = (Graphics2D) g;
         int xPosition;
+		
+		if(settingsAccessed) {
+		colorsRect.setSize(0, 0);
+		colorsPlayerRect.setSize(0, 0);
+		colorsBackRect.setSize(0, 0);
+		colorsEnemyRect.setSize(0, 0);
+		}
         
         saveRect = new Rectangle(275, 340, 150, 50);
         exitRect = new Rectangle(275, 400, 150, 50);
@@ -193,5 +260,62 @@ public class MenuPanel extends javax.swing.JPanel implements ActionListener {
         * @return           The rectangle representing the "save score" button.
         */
         return saveRect;
+    }
+    
+    public Rectangle getColorsRect() {
+        /**
+        * Function tries to return the rectangle for the "change colors" button.
+		* If the button has not been created and it has been somehow clicked, 
+		* return exitRect to close the program.
+        * @precondition     Menu has been initialized.
+        * @return           The rectangle representing the "change colors".
+		* button.
+        */
+		if(colorsRect == null) return exitRect;
+		
+        return colorsRect;
+    }
+    
+    public Rectangle getColorsPlayerRect() {
+        /**
+        * Function tries to return the rectangle for the "player color" button.
+		* If the button has not been created and it has been somehow clicked, 
+		* return exitRect to close the program.
+        * @precondition     Menu has been initialized.
+        * @return           The rectangle representing the "player color".
+		* button.
+        */
+		if(colorsPlayerRect == null) return exitRect;
+		
+        return colorsPlayerRect;
+    }
+    
+    public Rectangle getColorsBackRect() {
+        /**
+        * Function tries to return the rectangle for the "background color" 
+		* button.
+		* If the button has not been created and it has been somehow clicked, 
+		* return exitRect to close the program.
+        * @precondition     Menu has been initialized.
+        * @return           The rectangle representing the "background color".
+		* button.
+        */
+		if(colorsBackRect == null) return exitRect;
+		
+        return colorsBackRect;
+    }
+    
+    public Rectangle getColorsEnemyRect() {
+        /**
+        * Function tries to return the rectangle for the "enemy color" button.
+		* If the button has not been created and it has been somehow clicked, 
+		* return exitRect to close the program.
+        * @precondition     Menu has been initialized.
+        * @return           The rectangle representing the "enemy color".
+		* button.
+        */
+		if(colorsEnemyRect == null) return exitRect;
+		
+        return colorsEnemyRect;
     }
 }
